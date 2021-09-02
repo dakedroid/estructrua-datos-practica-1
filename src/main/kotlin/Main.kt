a76e6e90a12a20226d7060d86171f140074ba155
dakedroid <kevin.molina.go@gmail.com>
2021-09-02 11:41:20 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main (  ) {
+
+    val nombre = "KEVIN MOLINA"
+    val edad = 24
+
+    val x = 1
+    val y = 5
+
+    val suma = x + y
+
+    if ( suma > 10 ){
+        println(suma)
+    }else if (suma < 10){
+        println("no se puede imprimir por que es menor")
+    }
+}
+
+fun cocinar(){
+    println("Concinando")
+    println("Se termino de cocinar")
+}
+
+fun limpiarCocinar(){
+    println("Limpiar Cocina")
+}
+
+
+
